b0cb2d6f4a20862a9b9bc28a90f829ea6a163ad5
Balgor18 <catinaud.florian@gmail.com>
2020-03-16 09:59:46 +0100

    First commit

---
+++ src/components/LesTablesMultiplication.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="table" v-for="m in 13" v-bind:key="m" >
+      <h3>Table de {{m}}</h3>
+      <table class="table-multiplication">
+        <tbody>
+          <div v-for="k in 10" v-bind:key="k">
+            <TableRow v-bind:j="k" v-bind:i="m"/>
+          </div>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+  import TableRow from "./TableRow.vue";
+
+  export default {
+    name: "TableMultiplication",
+    props: {
+      n : Number
+    },
+    components: {
+      TableRow
+    },
+  }
+</script>
+<style scoped>
+  .table{
+    display: inline-block;
+    margin-left: 20px;
+  }
+</style>
---
+++ src/components/TableMultiplication.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h3>Table de {{n}}</h3>
+    <table class="table-multiplication" >
+      <tbody>
+        <div v-for="k in 10" v-bind:key="k">
+         <TableRow v-bind:j="k" v-bind:i="n"/>
+        </div>
+      </tbody>
+    </table>
+    <br/>
+  </div>
+</template>
+
+<script>
+  import TableRow from "./TableRow.vue";
+
+  export default {
+    name: "TableMultiplication",
+    props: {
+      n : Number
+    },
+    components: {
+      TableRow
+    },
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .table-multiplication {
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: lightblue;
+  }
+</style>
